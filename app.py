22bd0d1efbcaad21dc710a49f012a0254c24d047
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-14 17:23:11 -0500

    add logging and webhook

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 from decouple import config
 from pathlib import Path
+import logging
 
 dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
@@ -32,12 +33,23 @@ def boop(bot, update):
 
 
 def main():
-    updater = Updater(config('API_TOKEN'))
+    updater = Updater(config('API_KEY'))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('boop',boop))
     dp.add_handler(CommandHandler('joke', joke))
     updater.start_polling()
     updater.idle()
 
+
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     main()
+
+    updater.start_webhook(listen="0.0.0.0",
+                        port=int(config('PORT')),
+                        url_path=config("API_TOKEN"))
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(config("NAME"), config("TOKEN")))
+    updater.idle()
